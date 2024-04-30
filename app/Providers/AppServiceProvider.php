0750e8b745b860aad09a5221bfda3f6a924ebda0
Diego Andres Quevedo <91376899+DiegoQuevedoTIC@users.noreply.github.com>
2024-04-30 17:30:59 -0500

    Ubicacion Geografica Menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,17 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
-        //
+
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+
     public function boot(): void
     {
         Model::unguard();
